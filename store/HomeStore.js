e9b6a68ceaf56f5492ccab79cd95755bf5273281
Samyak Chopra <73484421+Samyak-Coder@users.noreply.github.com>
2026-02-01 07:18:21 +0530

    addHabbit changed according to the data type

--- store/HomeStore.js
+++ store/HomeStore.js
@@ -2,6 +2,6 @@ import {create} from 'zustand';
 
 export const useHabbitsStore = create((set)=>({
     habbits: [],
-    addHabbit: (habbit) => set((state) => ({habbits: [...state.habbits,  ...habbit]})),
+    addHabbit: (habbit) => set((state) => ({habbits: [...state.habbits,  habbit]})),
     rmHabbit: (habbitId) => set((state)=>({habbits: state.habbits.filter((habbit)=> habbit.id!=habbitId)}))
 }))
